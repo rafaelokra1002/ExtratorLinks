fa4a2d3f87ba3b95d13734a8b7e8b6bfd06f8602
rafaelokra1002 <rafaelokra1002@gmail.com>
2026-04-26 00:28:26 -0300

    Serve frontend assets from root handler

--- server.js
+++ server.js
@@ -2,6 +2,20 @@ const fs = require("fs");
 const path = require("path");
 
 const INDEX_PATH = path.join(__dirname, "index.html");
+const PUBLIC_DIR = path.join(__dirname, "public");
+
+function enviarArquivo(res, filePath, contentType, method) {
+  const conteudo = fs.readFileSync(filePath);
+  res.statusCode = 200;
+  res.setHeader("Content-Type", contentType);
+
+  if (method === "HEAD") {
+    res.end();
+    return;
+  }
+
+  res.end(conteudo);
+}
 
 module.exports = function handler(req, res) {
   if (req.method !== "GET" && req.method !== "HEAD") {
@@ -12,16 +26,27 @@ module.exports = function handler(req, res) {
   }
 
   try {
-    const html = fs.readFileSync(INDEX_PATH, "utf-8");
-    res.statusCode = 200;
-    res.setHeader("Content-Type", "text/html; charset=utf-8");
-
-    if (req.method === "HEAD") {
-      res.end();
+    if (req.url === "/styles.css") {
+      enviarArquivo(
+        res,
+        path.join(PUBLIC_DIR, "styles.css"),
+        "text/css; charset=utf-8",
+        req.method
+      );
       return;
     }
 
-    res.end(html);
+    if (req.url === "/app.js") {
+      enviarArquivo(
+        res,
+        path.join(PUBLIC_DIR, "app.js"),
+        "application/javascript; charset=utf-8",
+        req.method
+      );
+      return;
+    }
+
+    enviarArquivo(res, INDEX_PATH, "text/html; charset=utf-8", req.method);
   } catch (erro) {
     console.error("server.js error", erro);
     res.statusCode = 500;
